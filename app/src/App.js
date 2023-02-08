d198a931d7db0148749c1e34a965e7da08d4e5c2
zuomiaohu <hzm5622@126.com>
2023-02-08 15:41:40 -0500

    format

--- app/src/App.js
+++ app/src/App.js
@@ -1,88 +1,88 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css'
-import { Button, message, Steps, theme, Typography } from 'antd';
+import {Button, message, Steps, theme, Typography} from 'antd';
 
-import { MyLocation } from './components/MyLocation'
-import { ElectricityLoadEstimation } from './components/ElectricityLoadEstimation'
-import { EstimationParameters } from './components/EstimationParameters'
-import { Results } from './components/Results'
-import { SolarPanelParameters } from './components/SolarPanelParameters'
+import {MyLocation} from './components/MyLocation'
+import {ElectricityLoadEstimation} from './components/ElectricityLoadEstimation'
+import {EstimationParameters} from './components/EstimationParameters'
+import {Results} from './components/Results'
+import {SolarPanelParameters} from './components/SolarPanelParameters'
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 const steps = [
-  {
-    title: 'My Location',
-    content: MyLocation,
-  },
-  {
-    title: 'Solar Panel Parameters',
-    content: SolarPanelParameters,
-  },
-  {
-    title: 'Electricity Load Estimation',
-    content: ElectricityLoadEstimation,
-  },
-  {
-    title: 'Estimation Parameters',
-    content: EstimationParameters,
-  },
-  {
-    title: 'Results',
-    content: Results,
-  },
+    {
+        title: 'My Location',
+        content: MyLocation,
+    },
+    {
+        title: 'Solar Panel Parameters',
+        content: SolarPanelParameters,
+    },
+    {
+        title: 'Electricity Load Estimation',
+        content: ElectricityLoadEstimation,
+    },
+    {
+        title: 'Estimation Parameters',
+        content: EstimationParameters,
+    },
+    {
+        title: 'Results',
+        content: Results,
+    },
 ];
 
 const App = () => {
-  const { token } = theme.useToken();
-  const [current, setCurrent] = useState(0);
+    const {token} = theme.useToken();
+    const [current, setCurrent] = useState(0);
 
-  const next = () => {
-    setCurrent(current + 1);
-  };
+    const next = () => {
+        setCurrent(current + 1);
+    };
 
-  const prev = () => {
-    setCurrent(current - 1);
-  };
+    const prev = () => {
+        setCurrent(current - 1);
+    };
 
-  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+    const items = steps.map((item) => ({key: item.title, title: item.title}));
 
-  const contentStyle = {
-    lineHeight: '260px',
-    textAlign: 'center',
-    color: token.colorTextTertiary,
-    backgroundColor: token.colorFillAlter,
-    borderRadius: token.borderRadiusLG,
-    border: `1px dashed ${token.colorBorder}`,
-    marginTop: 16,
-  };
-  const onChange = (value) => {
-    setCurrent(value);
-  };
-  return (
-    <>
-      <Typography><Title level={2}> Solar Panel and Battery Size Calculator </Title></Typography>
-      <Steps current={current} items={items} onChange={onChange} />
-      <div style={contentStyle}>{steps[current].content()}</div>
-      <div style={{ marginTop: 24 }}>
-        {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()}>
-            Next
-          </Button>
-        )}
-        {current === steps.length - 1 && (
-          <Button type="primary" onClick={() => message.success('Processing complete!')}>
-            Done
-          </Button>
-        )}
-        {current > 0 && (
-          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-            Previous
-          </Button>
-        )}
-      </div>
-    </>
-  );
+    const contentStyle = {
+        lineHeight: '260px',
+        textAlign: 'center',
+        color: token.colorTextTertiary,
+        backgroundColor: token.colorFillAlter,
+        borderRadius: token.borderRadiusLG,
+        border: `1px dashed ${token.colorBorder}`,
+        marginTop: 16,
+    };
+    const onChange = (value) => {
+        setCurrent(value);
+    };
+    return (
+        <>
+            <Typography><Title level={2}> Solar Panel and Battery Size Calculator </Title></Typography>
+            <Steps current={current} items={items} onChange={onChange}/>
+            <div style={contentStyle}>{steps[current].content()}</div>
+            <div style={{marginTop: 24}}>
+                {current < steps.length - 1 && (
+                    <Button type="primary" onClick={() => next()}>
+                        Next
+                    </Button>
+                )}
+                {current === steps.length - 1 && (
+                    <Button type="primary" onClick={() => message.success('Processing complete!')}>
+                        Done
+                    </Button>
+                )}
+                {current > 0 && (
+                    <Button style={{margin: '0 8px'}} onClick={() => prev()}>
+                        Previous
+                    </Button>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default App;
--- app/src/components/MyLocation.js
+++ app/src/components/MyLocation.js
@@ -1,53 +1,75 @@
-import { Card } from 'antd';
-import { Divider } from 'antd';
-import { Button, Input, InputNumber } from 'antd';
-import { useState } from 'react';
+import {Card} from 'antd';
+import {Divider} from 'antd';
+import {Button, Input, InputNumber} from 'antd';
+import {useState} from 'react';
+
 export const MyLocation = () => {
-    const [status1, setStatus1] = useState('error') 
-    const [status2, setStatus2] = useState('error') 
+
+    const [status1, setStatus1] = useState('error')
+    const [status2, setStatus2] = useState('error')
     return <div>
-        <Card style={{ width: '90%', margin: '50px', textAlign: 'left' }}>
+        <Card style={{width: '90%', margin: '50px', textAlign: 'left'}}>
             <h1>Welcome!</h1>
             <p>
-                This calculator is intended for homeowners and small to medium businesses to determine how many solar panels and how large a storage battery to buy to achieve a certain level of grid independence, based on your location, solar panel parameters, and electricity usages.
+                This calculator is intended for homeowners and small to medium businesses to determine how many solar
+                panels and how large a storage battery to buy to achieve a certain level of grid independence, based on
+                your location, solar panel parameters, and electricity usages.
             </p>
 
             <p>
-                This calculator uses <a href="https://cs.uwaterloo.ca/~fkazhami/papers/synthetic_trace_generation.pdf">machine learning</a> to estimate your hourly electricity usage and a <a href="https://cs.uwaterloo.ca/~fkazhami/papers/robust-practical-approaches-8.pdf">robust statistical algorithm</a> to optimize the amount of solar panels and battery storage needed to fulfill a certain portion of your electricity needs with minimum cost.
+                This calculator uses <a href="https://cs.uwaterloo.ca/~fkazhami/papers/synthetic_trace_generation.pdf">machine
+                learning</a> to estimate your hourly electricity usage and a <a
+                href="https://cs.uwaterloo.ca/~fkazhami/papers/robust-practical-approaches-8.pdf">robust statistical
+                algorithm</a> to optimize the amount of solar panels and battery storage needed to fulfill a certain
+                portion of your electricity needs with minimum cost.
             </p>
 
             <p>
                 To use this calculator, please prepare the following:
-                <li className="li-dot"><b>Your location</b>, in terms of longitude and latitude. Later this page you can also detect your location with your IP address or enter your city.</li>
-                <li className="li-dot"><b>Solar panel parameters</b>, including the tilt and azimuth of your solar panel, and what types of panels you intend to install. See detailed instruction at page 2. The parameters and instructions are provided by <a href="https://pvwatts.nrel.gov/pvwatts.php">PVWatts</a>. </li>
-                <li className="li-dot"><b>Your electricity statements</b>, up to a year for each month. We are interested in how much electricity (in kWh) you used for each month during the past year, as well as (optionally) the electricity cost for the last entire year.</li>
-                <li className="li-dot"><b>Your local system costs</b> for solar panels and batteries. We listed out some sample prices per unit in the US but different region has different costs. <a href="https://www.energysage.com/solar/">EnergySage</a> provides great instructions and example prices.</li>
+                <li className="li-dot"><b>Your location</b>, in terms of longitude and latitude. Later this page you can
+                    also detect your location with your IP address or enter your city.</li>
+                <li className="li-dot"><b>Solar panel parameters</b>, including the tilt and azimuth of your solar
+                    panel, and what types of panels you intend to install. See detailed instruction at page 2. The
+                    parameters and instructions are provided by <a
+                        href="https://pvwatts.nrel.gov/pvwatts.php">PVWatts</a>.
+                </li>
+                <li className="li-dot"><b>Your electricity statements</b>, up to a year for each month. We are
+                    interested in how much electricity (in kWh) you used for each month during the past year, as well as
+                    (optionally) the electricity cost for the last entire year.
+                </li>
+                <li className="li-dot"><b>Your local system costs</b> for solar panels and batteries. We listed out some
+                    sample prices per unit in the US but different region has different costs. <a
+                        href="https://www.energysage.com/solar/">EnergySage</a> provides great instructions and example
+                    prices.
+                </li>
             </p>
         </Card>
-        <Card style={{ width: '90%', margin: '50px', textAlign: 'center' }}>
-            <h1 style={{ textAlign: 'left' }}>Your Location</h1>
+        <Card style={{width: '90%', margin: '50px', textAlign: 'center'}}>
+            <h1 style={{textAlign: 'left'}}>Your Location</h1>
             <p>
-                <b>Location</b>: Your location is needed to compute how much electricity your solar panels can generate. Enter your locations using one of the following ways:
+                <b>Location</b>: Your location is needed to compute how much electricity your solar panels can generate.
+                Enter your locations using one of the following ways:
             </p>
 
             <p>
-                <b>Autofill location using your IP address:</b>                
-                <Button style={{marginLeft:'10px'}}>Detect Location</Button>
+                <b>Autofill location using your IP address:</b>
+                <Button style={{marginLeft: '10px'}}>Detect Location</Button>
             </p>
-            
+
             <Divider>OR</Divider>
 
             <p>
-                <b>Enter your city:</b>  
-                <Input style={{width:'200px',borderBottom:'1px solid #000', marginLeft:'10px',borderRadius:0}} placeholder="your city" bordered={false}/>
+                <b>Enter your city:</b>
+                <Input style={{width: '200px', borderBottom: '1px solid #000', marginLeft: '10px', borderRadius: 0}}
+                       placeholder="your city" bordered={false}/>
             </p>
             <Divider>OR</Divider>
 
             <p>
                 <b>Enter your latitude and longitude: </b>
-                <InputNumber 
-                    style={{width:'100px', marginLeft:'10px'}} 
-                    placeholder="latitude" 
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    placeholder="latitude"
                     status={status1}
                     min="-90"
                     max="90"
@@ -58,12 +80,12 @@ export const MyLocation = () => {
                         } else {
                             setStatus1('error')
                         }
-                        
+
                     }}
                     stringMode
                 />
-                <InputNumber 
-                    style={{width:'100px', marginLeft:'10px'}} 
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
                     placeholder="longitude"
                     status={status2}
                     min="0"

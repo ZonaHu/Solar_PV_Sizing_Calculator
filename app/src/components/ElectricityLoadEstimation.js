039f918c6a7664a7a7eb757b88aa1066058ab1e7
zuomiaohu <hzm5622@126.com>
2023-03-02 23:36:53 -0500

    init electricity load estimation

--- app/src/components/ElectricityLoadEstimation.js
+++ app/src/components/ElectricityLoadEstimation.js
@@ -1,3 +1,155 @@
+import {Card, Input, InputNumber, Typography} from "antd";
+
+
+const {Title, Paragraph} = Typography;
+
 export const ElectricityLoadEstimation = () => {
-    return <div>ElectricityLoadEstimation</div>
+
+    return <div>
+        <Title level={4} style={{padding: '50px 50px 0px', textAlign: 'left'}}> Enter following information for your electricity
+            estimation.</Title>
+        <Card style={{width: '90%', margin: '50px', textAlign: 'left'}}>
+            <Title level={3} style={{textAlign: 'left'}}>Electric Vehicle</Title>
+
+            <Paragraph>
+                <b>Enter the number of electric vehicle you have: </b>
+                <InputNumber
+                    style={{width: '50px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+
+            <Paragraph>
+                <b> How many of them are bidirectional? </b>
+                <InputNumber
+                    style={{width: '50px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+
+            {/*<Paragraph>*/}
+            {/*    <b> How large is the battery in kWh? </b>*/}
+            {/*    <InputNumber*/}
+            {/*        style={{width: '50px', marginLeft: '10px'}}*/}
+            {/*        min="0"*/}
+            {/*    />*/}
+            {/*</Paragraph>*/}
+            <Paragraph>
+                <b>How many days per week do you commute to work with EV? </b>
+                <InputNumber
+                    style={{width: '50px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+
+            <Paragraph>
+                <b> When does the EV usually leave the house</b>
+                <Input
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+
+            <Paragraph>
+                <b> When does the EV usually return the house</b>
+                <Input
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+
+            <Paragraph>
+                <b> What is the state of charge of the EV battery when you arrive?</b>
+                <Input
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+        </Card>
+
+        <Card style={{width: '90%', margin: '50px', textAlign: 'left'}}>
+            <Title level={3} style={{textAlign: 'left'}}>Light Style</Title>
+
+            Need to confirm what are the required parameters.
+
+        </Card>
+
+        <Card style={{width: '90%', margin: '50px', textAlign: 'left'}}>
+            <Title level={3} style={{textAlign: 'left'}}>Panel Replacement</Title>
+
+            Need to confirm what are the required parameters.
+
+        </Card>
+
+        <Card style={{width: '90%', margin: '50px', textAlign: 'left'}}>
+            <Title level={3} style={{textAlign: 'left'}}>Monthly Electricity Load in kWh</Title>
+
+            <Paragraph>
+                <b> January * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> February * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> March * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> April * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+            <Paragraph>
+                <b> May * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> June * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> July * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> August * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+            <Paragraph>
+                <b> September * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> October * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> November * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+                <b style={{ marginLeft: '10px'}}> December * </b>
+                <InputNumber
+                    style={{width: '100px', marginLeft: '10px'}}
+                    min="0"
+                />
+            </Paragraph>
+        </Card>
+    </div>
 }
